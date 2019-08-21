8a232b8b49316a377cb2f8504740f8974dcba1da
kenchangh <guanhao3797@gmail.com>
2019-08-21 15:59:49 +0800

    reformat test

--- test/erasure.js
+++ test/erasure.js
@@ -1,34 +1,43 @@
-const contracts = require('./helpers/contracts');
-const constants = require('./helpers/constants');
-const etherlime = require('etherlime-lib');
+const contracts = require("./helpers/contracts");
+const constants = require("./helpers/constants");
+const etherlime = require("etherlime-lib");
 
-const utils = require('ethers').utils;
+const utils = require("ethers").utils;
 
+describe("Test Erasure agreements", async () => {
+  let agreement;
+  let mockNmr;
+  const userAddress = "0x0000000000000000000000000000000000000021";
 
-describe('Test Erasure agreements', async () => {
-    let agreement;
-    let mockNmr;
-    const userAddress = '0x0000000000000000000000000000000000000021';
+  beforeEach(async () => {
+    agreement = await contracts.deployAgreement(
+      userAddress,
+      constants.multiSigWallet
+    );
+    mockNmr = await contracts.getMockNMR();
 
-    beforeEach(async () => {
-        agreement = await contracts.deployAgreement(userAddress, constants.multiSigWallet);
-        mockNmr = await contracts.getMockNMR();
+    await mockNmr.transfer(userAddress, utils.parseEther("100"));
+    await mockNmr.transfer(constants.multiSigWallet, utils.parseEther("100"));
+  });
 
-        await mockNmr.transfer(userAddress, utils.parseEther("100"));
-        await mockNmr.transfer(constants.multiSigWallet, utils.parseEther("100"));
-    });
+  it("should stake", async () => {
+    const contract = contracts.contractFrom(mockNmr, userAddress);
+    let txn = await contract.approve(
+      agreement.contractAddress,
+      utils.parseEther("1")
+    );
+    // const receipt = await mockNmr.verboseWaitForTransaction(txn);
+    console.log("txn", txn);
+    txn = await contracts
+      .contractFrom(agreement, userAddress)
+      .increaseStake(0, utils.parseEther("1"));
+  });
 
-    it('should stake', async () => {
-        const contract = contracts.contractFrom(mockNmr, userAddress);
-        let txn = await contract.approve(agreement.contractAddress, utils.parseEther("1"));
-        // const receipt = await mockNmr.verboseWaitForTransaction(txn);
-        console.log("txn", txn);
-        txn = await contracts.contractFrom(agreement, userAddress).increaseStake(0, utils.parseEther("1"));
-    });
-
-    it('should reward', async () => {
-        const contract = contracts.contractFrom(mockNmr, constants.multiSigWallet);
-        await contract.approve(agreement.contractAddress, utils.parseEther("1"));
-        const txn = await contracts.contractFrom(agreement, constants.multiSigWallet).reward(0, utils.parseEther("1"));
-    });
+  it("should reward", async () => {
+    const contract = contracts.contractFrom(mockNmr, constants.multiSigWallet);
+    await contract.approve(agreement.contractAddress, utils.parseEther("1"));
+    const txn = await contracts
+      .contractFrom(agreement, constants.multiSigWallet)
+      .reward(0, utils.parseEther("1"));
+  });
 });
--- test/helpers/contracts.js
+++ test/helpers/contracts.js
@@ -1,130 +1,156 @@
-const constants = require('./constants');
+const constants = require("./constants");
 
-const etherlime = require('etherlime-lib');
-const utils = require('ethers').utils;
-const ethers = require('ethers');
+const etherlime = require("etherlime-lib");
+const utils = require("ethers").utils;
+const ethers = require("ethers");
 
-const NumeraiTournament = require('../../build/NumeraiTournamentV3.json');
-const Relay = require('../../build/Relay.json');
-const MockNMR = require('../../build/MockNMR.json');
-const OneWayGriefing = require('../../build/OneWayGriefingNoCountdown.json');
+const NumeraiTournament = require("../../build/NumeraiTournamentV3.json");
+const Relay = require("../../build/Relay.json");
+const MockNMR = require("../../build/MockNMR.json");
+const OneWayGriefing = require("../../build/OneWayGriefingNoCountdown.json");
 
 async function increaseNonce(signer, increaseTo) {
-    const currentNonce = await signer.getTransactionCount();
-    if (currentNonce === increaseTo) {
-        return;
-    }
-    if (currentNonce > increaseTo) {
-        throw new Error(`nonce is greater than desired value ${currentNonce} > ${increaseTo}`);
-    }
+  const currentNonce = await signer.getTransactionCount();
+  if (currentNonce === increaseTo) {
+    return;
+  }
+  if (currentNonce > increaseTo) {
+    throw new Error(
+      `nonce is greater than desired value ${currentNonce} > ${increaseTo}`
+    );
+  }
 
-    for (let index = 0; index < increaseTo - currentNonce; index++) {
-        const transaction = {
-            to: constants.multiSigWallet, // just send to a random address, it doesn't really matter who
-            value: utils.parseEther("0.0000000000001"),
-        }
-        await signer.sendTransaction(transaction);
-    }
+  for (let index = 0; index < increaseTo - currentNonce; index++) {
+    const transaction = {
+      to: constants.multiSigWallet, // just send to a random address, it doesn't really matter who
+      value: utils.parseEther("0.0000000000001")
+    };
+    await signer.sendTransaction(transaction);
+  }
 }
 async function fundEth(to) {
-    deployer = new etherlime.EtherlimeGanacheDeployer(constants.fundedAccountPrivateKey);
+  deployer = new etherlime.EtherlimeGanacheDeployer(
+    constants.fundedAccountPrivateKey
+  );
 
-    let transaction = {
-        to: to,
-        value: utils.parseEther("1.0"),
-    }
+  let transaction = {
+    to: to,
+    value: utils.parseEther("1.0")
+  };
 
-    await deployer.signer.sendTransaction(transaction);
+  await deployer.signer.sendTransaction(transaction);
 }
 
 let _mockNmrContract;
 async function _deployMockNMR() {
-    const deployAddress = constants.nmrTokenDeployer;
-    await fundEth(deployAddress);
+  const deployAddress = constants.nmrTokenDeployer;
+  await fundEth(deployAddress);
 
-    const deployer = new etherlime.EtherlimeGanacheDeployer(constants.fundedAccountPrivateKey);
-    deployer.signer = deployer.provider.getSigner(deployAddress);
-    await increaseNonce(deployer.signer, 1);
+  const deployer = new etherlime.EtherlimeGanacheDeployer(
+    constants.fundedAccountPrivateKey
+  );
+  deployer.signer = deployer.provider.getSigner(deployAddress);
+  await increaseNonce(deployer.signer, 1);
 
-    const contract = await deployer.deploy(MockNMR, {});
+  const contract = await deployer.deploy(MockNMR, {});
 
-    return contract;
+  return contract;
 }
 async function getMockNMR() {
-    if (!_mockNmrContract) {
-        _mockNmrContract = _deployMockNMR();
-    }
+  if (!_mockNmrContract) {
+    _mockNmrContract = _deployMockNMR();
+  }
 
-    return await _mockNmrContract;
+  return await _mockNmrContract;
 }
 
 let _tournamentContract;
 async function _deployTournament() {
-    const deployAddress = constants.multiSigWallet;
+  const deployAddress = constants.multiSigWallet;
 
-    await fundEth(deployAddress);
+  await fundEth(deployAddress);
 
-    const deployer = new etherlime.EtherlimeGanacheDeployer(constants.fundedAccountPrivateKey);
-    deployer.signer = deployer.provider.getSigner(deployAddress);
-    await increaseNonce(deployer.signer, 23);
+  const deployer = new etherlime.EtherlimeGanacheDeployer(
+    constants.fundedAccountPrivateKey
+  );
+  deployer.signer = deployer.provider.getSigner(deployAddress);
+  await increaseNonce(deployer.signer, 23);
 
-    const contract = await deployer.deploy(NumeraiTournament, {});
+  const contract = await deployer.deploy(NumeraiTournament, {});
 
-    await contract.initialize(deployAddress);
+  await contract.initialize(deployAddress);
 
-    return contract;
+  return contract;
 }
 async function getTournament() {
-    if (!_tournamentContract) {
-        _tournamentContract = _deployTournament();
-    }
+  if (!_tournamentContract) {
+    _tournamentContract = _deployTournament();
+  }
 
-    return await _tournamentContract;
+  return await _tournamentContract;
 }
 
 let _relayContract;
 async function _deployRelay() {
-    const deployAddress = constants.multiSigWallet;
+  const deployAddress = constants.multiSigWallet;
 
-    await fundEth(deployAddress);
+  await fundEth(deployAddress);
 
-    const deployer = new etherlime.EtherlimeGanacheDeployer(constants.fundedAccountPrivateKey);
-    deployer.signer = deployer.provider.getSigner(deployAddress);
-    await increaseNonce(deployer.signer, 5);
+  const deployer = new etherlime.EtherlimeGanacheDeployer(
+    constants.fundedAccountPrivateKey
+  );
+  deployer.signer = deployer.provider.getSigner(deployAddress);
+  await increaseNonce(deployer.signer, 5);
 
-    const contract = await deployer.deploy(Relay, {}, constants.tournamenContractAddress);
+  const contract = await deployer.deploy(
+    Relay,
+    {},
+    constants.tournamenContractAddress
+  );
 
-    return contract;
+  return contract;
 }
 async function getRelay() {
-    if (!_relayContract) {
-        _relayContract = _deployRelay();
-    }
+  if (!_relayContract) {
+    _relayContract = _deployRelay();
+  }
 
-    return await _relayContract;
+  return await _relayContract;
 }
 
 async function deployAgreement(staker, counterparty) {
-    const deployer = new etherlime.EtherlimeGanacheDeployer(constants.fundedAccountPrivateKey);
+  const deployer = new etherlime.EtherlimeGanacheDeployer(
+    constants.fundedAccountPrivateKey
+  );
 
-    const contract = await deployer.deploy(OneWayGriefing, {});
-    // 4 = Inf griefing type
-    await contract.initialize(constants.nmrContractAddress, ethers.constants.AddressZero, staker, counterparty, 0, 4, '0x0');
-    return contract;
+  const contract = await deployer.deploy(OneWayGriefing, {});
+  // 4 = Inf griefing type
+  await contract.initialize(
+    constants.nmrContractAddress,
+    ethers.constants.AddressZero,
+    staker,
+    counterparty,
+    0,
+    4,
+    "0x0"
+  );
+  return contract;
 }
 
 // It turns out etherlime only supports calling `contract.from` on the built in addresses
 // For custom addresses such as the multisig wallet, we have to use the following instead
 function contractFrom(contract, address) {
-    const deployer = new etherlime.EtherlimeGanacheDeployer(constants.fundedAccountPrivateKey);
-    const signer = deployer.provider.getSigner(address);
-    return contract.contract.connect(signer);
+  const deployer = new etherlime.EtherlimeGanacheDeployer(
+    constants.fundedAccountPrivateKey
+  );
+  const signer = deployer.provider.getSigner(address);
+  return contract.contract.connect(signer);
 }
 
 module.exports = {
-    getMockNMR,
-    getTournament,
-    getRelay,
-    deployAgreement,
-    contractFrom,
+  getMockNMR,
+  getTournament,
+  getRelay,
+  deployAgreement,
+  contractFrom
 };
