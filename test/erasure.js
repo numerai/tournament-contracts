5e2676ddea1a1f913afd23fa5ba5766b9061d018
Jason Paryani <github@jparyani.com>
2019-10-09 17:48:05 -0700

    Add initial version of NumeraiErasureV1

--- test/erasure.js
+++ test/erasure.js
@@ -1,30 +1,80 @@
 const contracts = require('./helpers/contracts');
-const constants = require('./helpers/constants');
-const etherlime = require('etherlime-lib');
 
 const utils = require('ethers').utils;
+
+const SimpleGriefing = require('../build/SimpleGriefing.json');
 
 
 describe('Test Erasure agreements', async () => {
     let agreement;
+    let factory;
     let mockNMRContract;
-    let tournamentContract;
+    let numeraiErasureContract;
     const userAddress = '0x0000000000000000000000000000000000000021';
 
-    beforeEach(async () => {
-        agreement = await contracts.deployAgreement(userAddress, constants.multiSigWallet, constants.tournamenContractAddress);
+    before(async () => {
+        factory = await contracts.deployAgreementFactory();
         mockNMRContract = await contracts.getMockNMR();
-        tournamentContract = await contracts.getTournament();
+        numeraiErasureContract = await contracts.deployNumeraiErasureV1();
+        const relay = await contracts.getRelay();
+        await relay.transferManagement(numeraiErasureContract.contractAddress);
 
         await mockNMRContract.transfer(userAddress, utils.parseEther("100"));
     });
 
-    it('should increaseStake', async () => {
+    it('should createStake', async () => {
         const stakeAmount = utils.parseEther("10");
-        let txn = await tournamentContract.increaseStakeErasure(agreement.contractAddress, userAddress, 0, stakeAmount);
-        const receipt = await tournamentContract.verboseWaitForTransaction(txn);
+        let salt = utils.formatBytes32String("should createStake");
+        const callData = contracts.createSimpleGriefingCallData(userAddress, numeraiErasureContract.contractAddress, numeraiErasureContract.contractAddress);
+
+        const agreementAddress = await factory.getSaltyInstance(callData, salt);
+
+        let txn = await numeraiErasureContract.createStake(factory.contractAddress, agreementAddress, userAddress, stakeAmount, callData, salt);
+
+        const receipt = await numeraiErasureContract.verboseWaitForTransaction(txn);
         const stakeEvent = receipt.events.find(
-            emittedEvent => emittedEvent.event === "IncreaseStakeErasure",
+            emittedEvent => emittedEvent.event === "IncreaseStake",
+            "There is no increaseStake event"
+        );
+
+        assert.isDefined(stakeEvent);
+        assert.equal(stakeEvent.args.agreement, agreementAddress);
+        assert.equal(stakeEvent.args.staker, userAddress);
+        assert.strictEqual(
+            stakeEvent.args.amountAdded.toString(),
+            stakeAmount.toString(),
+        );
+        assert.strictEqual(
+            stakeEvent.args.oldStakeAmount.toString(),
+            "0",
+        );
+
+        const stakeEventCreate = receipt.events.find(
+            emittedEvent => emittedEvent.event === "CreateStake",
+            "There is no createStake event"
+        );
+
+        assert.isDefined(stakeEventCreate);
+        assert.equal(stakeEventCreate.args.agreement, agreementAddress);
+        assert.equal(stakeEventCreate.args.staker, userAddress);
+        assert.strictEqual(
+            stakeEventCreate.args.amount.toString(),
+            stakeAmount.toString(),
+        );
+
+        let balance = await mockNMRContract.balanceOf(userAddress);
+        assert.strictEqual(balance.toString(), utils.parseEther("90").toString(), 'balance is wrong for user');
+
+        agreement = deployer.wrapDeployedContract(SimpleGriefing, agreementAddress);
+    });
+
+    it('should increaseStake', async () => {
+        const stakeAmount = utils.parseEther("15");
+        const oldStakeAmount = utils.parseEther("10");
+        let txn = await numeraiErasureContract.increaseStake(agreement.contractAddress, userAddress, oldStakeAmount, stakeAmount);
+        const receipt = await numeraiErasureContract.verboseWaitForTransaction(txn);
+        const stakeEvent = receipt.events.find(
+            emittedEvent => emittedEvent.event === "IncreaseStake",
             "There is no such event"
         );
 
@@ -37,10 +87,10 @@ describe('Test Erasure agreements', async () => {
         );
         assert.strictEqual(
             stakeEvent.args.oldStakeAmount.toString(),
-            "0",
+            oldStakeAmount.toString(),
         );
 
         let balance = await mockNMRContract.balanceOf(userAddress);
-        assert.strictEqual(balance.toString(), utils.parseEther("90").toString(), 'balance is wrong for user');
+        assert.strictEqual(balance.toString(), utils.parseEther("75").toString(), 'balance is wrong for user');
     });
 });
