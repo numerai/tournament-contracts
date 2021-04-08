f04ffcded404e66e8fc5631335a4d41aa756cd91
Noah Harasz <ndharasz@live.com>
2021-04-08 12:38:32 -0400

    fix local testing and document it (#52)

--- run-ganache-deploy.sh
+++ run-ganache-deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-set -eu
+set -e
 
-# kill ganache on exit
-trap 'kill $(jobs -p)' EXIT
-
+# run ganache in background
 yarn run ganache > ganache.log &
+PID="$!"
+sleep 5
 
 OUTPUT="$(yarn run deploy-numeraierasure ganache 2>&1)"
 
 echo -e "$OUTPUT"
 
-# exit error if failure found in output
-if echo $OUTPUT | grep -q 'failure'; then
+kill $PID
+
+## exit error if failure found in output
+if (echo "$OUTPUT" | grep -c 'failure') > 0; then
+  echo "detected failure..."
   exit 1
 fi
